0126d1f12ec01cde32153d4a8e8b122716acd6d6
AdhikowshikJ <adhireddy716@gmail.com>
2024-03-04 18:58:07 +0530

    LeftBar fixed

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -30,62 +30,70 @@ const LeftBar = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const isLarge = useMediaQuery('(min-width:1200px)')
+  const isMobile=useMediaQuery('(max-width:600px)')
   const handleClick=()=>{}
   return (
-    <Box>
-   
-    <Drawer
-      variant="permanent"
-      sx={{
-        display: { xs: 'none', sm: 'block' },
-        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: { md: '100px', lg: '180px', sm: '90px' } },
-      }}
-      open
-    >
-     <Box height={64}>
-      hfhf
-    </Box>
-    <hr/>
-      <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', textAlign:'center'}}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
+     !isMobile ? (<Box>
      
-    > {routes.map((routeGroup) =>
-            Object.entries(routeGroup).map(([key, value]) => (
-              <ListItemButton key={key} sx={{margin:'10px'}}>
-              
-                <Link to={value.route}>
-                  <Box sx={{ display: 'flex',justifyContent:'center', alignItems: 'center' }}>
-                  <ListItemIcon >
-                  {value.logo}
-       </ListItemIcon>
-                    
-                    {isLarge && (
-                      <ListItemText sx={{ display: { sm: 'none', md: 'none', lg: 'block' } }} primary={value.label} />
-                    )}
-                  </Box>
-                </Link>
-              </ListItemButton>
-            ))
-          )}
-          <ListItemButton onClick={handleOpen} sx={{margin:'10px'}}>
-            <ListItemIcon>
-              <AddCircleOutlineOutlined/>
-            </ListItemIcon>
-            <ListItemText sx={{ display: { sm: 'none', md: 'none', lg: 'block' } }}>Create</ListItemText>
-          </ListItemButton>
-    </List>
-    </Drawer>
-    <Dialog
-        open={open}
-        onClose={handleClose}
+      <Drawer
+        variant="permanent"
+        sx={{
+       
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: { md: '100px', lg: '180px', sm: '90px' } },
+        }}
+        open
       >
-        <DialogActions >
-          <CreatePost/>
-        </DialogActions>
-      </Dialog>
-  </Box>
+       <Box height={64}>
+        hfhf
+      </Box>
+      <hr/>
+        <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', textAlign:'center'}}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+       
+      > {routes.map((routeGroup) =>
+              Object.entries(routeGroup).map(([key, value]) => (
+                
+                <Link to={value.route}>
+                <ListItemButton key={key} sx={{margin:'10px'}}>
+                
+                    <Box sx={{ display: 'flex',justifyContent:'center', alignItems: 'center' }}>
+                    <ListItemIcon >
+                    {value.logo}
+         </ListItemIcon>
+                      
+                      {isLarge && (
+                        <ListItemText sx={{ display: { sm: 'none', md: 'none', lg: 'block' } }} primary={value.label} />
+                      )}
+                    </Box>
+                 
+                </ListItemButton> </Link>
+              ))
+            )}
+            <ListItemButton onClick={handleOpen} sx={{margin:'10px'}}>
+              <ListItemIcon>
+                <AddCircleOutlineOutlined/>
+              </ListItemIcon>
+              <ListItemText sx={{ display: { sm: 'none', md: 'none', lg: 'block' } }}>Create</ListItemText>
+            </ListItemButton>
+      </List>
+      </Drawer>
+      <Dialog
+          open={open}
+          onClose={handleClose}
+        >
+          <DialogActions >
+            <CreatePost/>
+          </DialogActions>
+        </Dialog>
+    </Box>) : (
+      <Box position='fixed' bgcolor='black' width='100%' height='64px' bottom>
+
+
+      </Box>
+    )
+    
 
   )
 }
